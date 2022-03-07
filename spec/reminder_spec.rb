9fc7f82bf7049e60ac3c5329ee4046044ddef801
Orlane <orlanedoumbe@gmail.com>
2022-03-07 16:32:23 +0000

    Reminder with fail example

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,11 +1,19 @@
 require 'reminder'
 
 RSpec.describe Reminder do
+  context "when no task is set" do
+    it "fails" do
+      reminder = Reminder.new("Ashtray")
+      expect { reminder.remind() }.to raise_error "No reminder set!"
+    end
+
+
   it "reminds the user to do a task" do
     reminder = Reminder.new("Lexi")
     reminder.remind_me_to("Write the play")
     result = reminder.remind()
     expect(result).to eq "Write the play, Lexi!"
   end
+end
 end
 
